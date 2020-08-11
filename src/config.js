378728972b8ec536e70bf321c79a4b479ee70be2
Wayne Jones <waynejones@nyu.edu>
2020-08-11 19:22:40 -0400

    Installed Firebase

---
+++ src/config.js
@@ -0,0 +1,11 @@
+const firebaseConfig = {
+    apiKey: "AIzaSyBVU7Drhw6M-5dxSJc9m0kSJhW3R4f7TrQ",
+    authDomain: "debate-website-b170d.firebaseapp.com",
+    databaseURL: "https://debate-website-b170d.firebaseio.com",
+    projectId: "debate-website-b170d",
+    storageBucket: "debate-website-b170d.appspot.com",
+    messagingSenderId: "156457005313",
+    appId: "1:156457005313:web:ca319d98cb775c5572eb00"
+  };
+
+module.exports = firebaseConfig;
--- src/index.js
+++ src/index.js
@@ -1,8 +1,14 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
+import * as firebase from "firebase/app";
+import "firebase/auth";
+import "firebase/firestore";
+import firebaseConfig from './config';
+
 import App from './App';
 import * as serviceWorker from './serviceWorker';
+firebase.initializeApp(firebaseConfig);
 
 ReactDOM.render(
   <React.StrictMode>
