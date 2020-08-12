8dd80722124d46f5cd7baa20894a4540a582c626
Wayne Jones <waynejones@nyu.edu>
2020-08-12 14:30:06 -0400

    Added simple todo function with Firebase, updated firebase rules

--- src/App.js
+++ src/App.js
@@ -1,24 +1,34 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, {useState, useEffect} from 'react';
+import firebase from './config';
 import './App.css';
 
 function App() {
+  const [todoList, setToDoList] = useState([]);
+
+  const db = firebase.firestore();
+
+  const addToDo = async (e) =>{
+    e.preventDefault();
+    const inputVal = e.target.todo.value;
+    const res = await db.collection('todos').add({
+      todo: inputVal,
+    });
+    console.log('Added document with ID: ', res.id);
+    setToDoList([...todoList, inputVal]);
+  }
+  
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <form onSubmit={(e) => addToDo(e)}>
+        <input name="todo" type="text" placeholder="todo task"/>
+        <button type="submit">Submit Task</button>
+      </form>
+      
+      <div>
+        {todoList.map((todo)=>{
+          return <li key={todo._id}>{todo}</li>
+        })}
+      </div>
     </div>
   );
 }
